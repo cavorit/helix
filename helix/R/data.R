3b365edc77a69f826a7441729413ed31e38184a6
CGoalStatistics HF <cdo@cgoal.de>
2015-10-12 08:00:26 +0200

    Versuch, per roxygen2 INDER zu dokumentieren

---
+++ helix/R/data.R
@@ -0,0 +1,12 @@
+#' Messungen von 15 indischen U15 Fussballspieler.
+#'
+#' Der Datensatz enthält Messungen von 15 indischen U15-Spieler. Gemessen wurden u.A. Helix, Determinationstest und Footbonaut.
+#'
+#' @format Ein data.frame mit 15 Spalten und 15 Zeilen:
+#' \describe{
+#'   \item{Vpn}{Identifikator der Versuchsperson}
+#'   \item{Name}{Familienname}
+#'}
+#'
+#' 
+"INDER"
